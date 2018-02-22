ca3a356765aa904d193241adc0c66691c7edb9ed
OverworldLord <36016717+OverworldLord@users.noreply.github.com>
2018-02-21 21:19:38 -0800

    Modified testSaveFile.cpp / updated main
    testSaveFile is now a function, and not it's own main.
    
    Updates to rest of files.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
  * Mason Godfrey, Tristan Ligtvoet, Mitchell O'Hair, Kenneth Casimiro *
  * Mon,Wed 1:30p * Bulk Club * Document created during Sprint 1       *
  **********************************************************************/
-
 #include "header.h"
+#include "shoppers.h"
+
+bool login();
+void testSaveFile();
+void salesInformation();
+
+const bool SKIP_LOGIN = 0;
+
 using namespace std;
 
 int main() {
 
+	if(SKIP_LOGIN == false)
+	{
+		login();
+	}
+
+	testSaveFile();
+	salesInformation();
+
+	return 0;
 }
