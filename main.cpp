d0a57b90499c67a75ac1f56123971661e3075fda
OverworldLord <36016717+OverworldLord@users.noreply.github.com>
2018-02-22 20:15:56 -0800

    Added ability to make BC members/mod inventory
    Bulk club members can now be added (but not deleted).
    Purchases can now be made for the new members.
    Quantity of items can be added/removed from the inventory list.

--- main.cpp
+++ main.cpp
@@ -3,25 +3,32 @@
  * Mon,Wed 1:30p * Bulk Club * Document created during Sprint 1       *
  **********************************************************************/
 #include "header.h"
-#include "shoppers.h"
+using namespace std;
 
 bool login();
 void testSaveFile();
 void salesInformation();
+void testInventory();
+void tempShopperTester();
+void addNewShopper(string memNum);
 
 const bool SKIP_LOGIN = true;
 
-using namespace std;
-
 int main() {
+
+	string memNum = "00000";
 
 	if(SKIP_LOGIN == false)
 	{
 		login();
 	}
 
-	testSaveFile();
-	salesInformation();
+	addNewShopper(memNum);
+
+	//testInventory();
+	tempShopperTester();
+	//testSaveFile();
+	//salesInformation();
 
 	return 0;
 }
